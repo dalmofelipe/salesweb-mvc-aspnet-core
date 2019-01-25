42bff75c9e90f908ef75f6fb48b92016c58d8c66
Dalmo Felipe <dalmo.felipe@gmail.com>
2019-01-25 16:30:53 -0200

    primeiro controller e teste em razorpages

--- SaleWebMvc/Controllers/HomeController.cs
+++ SaleWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SaleWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web Curso CSharp";
+            ViewData["Email"] = "dfelipe@email.com";
 
             return View();
         }
