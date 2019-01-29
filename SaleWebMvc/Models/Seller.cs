e52980ed407226a297da964c89084d3b74745ea7
Dalmo Felipe <dalmo.felipe@gmail.com>
2019-01-29 12:41:20 -0200

    validacoes de formularios e ajute navbar

--- SaleWebMvc/Models/Seller.cs
+++ SaleWebMvc/Models/Seller.cs
@@ -10,18 +10,26 @@ namespace SaleWebMvc.Models
         public int Id { get; set; }
 
         [Display(Name = "Nome")]
+        [Required(ErrorMessage = "{0} é campo obrigatório")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "{0} deve possuir entre {2} e {1} caracteres")]
         public string Name { get; set; }
 
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} é um campo obrigatório!")]
+        [EmailAddress(ErrorMessage = "Entre com um email válido!")]
         public string Email { get; set; }
 
         [Display(Name = "Data de Nascimento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "{0} é campo obrigatório!")]
         public DateTime BirthDate { get; set; }
 
         [Display(Name = "Salário")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "{0} é campo obrigatório!")]
+        [Range(100.0, 5000.0, ConvertValueInInvariantCulture = true, 
+            ErrorMessage = "Informe um valor entre {2} e {1}")]
         public double BaseSalary { get; set; }
 
         public Department Department { get; set; }
