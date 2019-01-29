7c9d3fd37cafba12b39cc51798fe421092d883a8
Dalmo Felipe <dalmo.felipe@gmail.com>
2019-01-29 15:25:14 -0200

    impletando busca simples de vendas

--- SaleWebMvc/Data/SaleWebMvcContext.cs
+++ SaleWebMvc/Data/SaleWebMvcContext.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace SaleWebMvc.Models
 {
     public class SaleWebMvcContext : DbContext
     {
-        public SaleWebMvcContext (DbContextOptions<SaleWebMvcContext> options)
-            : base(options)
+        public SaleWebMvcContext (DbContextOptions<SaleWebMvcContext> options) : base(options)
         {
         }
 
---
+++ SaleWebMvc/Services/SalesRecordService.cs
@@ -0,0 +1,40 @@
+﻿using SaleWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace SaleWebMvc.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SaleWebMvcContext _context;
+
+        public SalesRecordService(SaleWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var query = from obj in _context.SalesRecord select obj;
+
+            if (minDate.HasValue)
+            {
+                query = query.Where(x => x.Date >= minDate);
+            }
+
+            if (maxDate.HasValue)
+            {
+                query = query.Where(x => x.Date <= maxDate);
+            }
+
+            return await query
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+    }
+}
